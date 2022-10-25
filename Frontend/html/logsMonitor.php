9acdb603ce7d13ac86b1b7cefde43b1572e8b5b0
BRRR11 <danielbarrientosbarrantes@gmail.com>
2022-10-25 15:12:47 -0600

    Refresh Automatico User & Logs

--- Frontend/html/logsMonitor.php
+++ Frontend/html/logsMonitor.php
@@ -29,6 +29,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="10" >
     <title>SGA Size Monitor</title>
     <link rel="stylesheet" href="tableuser.css">
     <!-- JavaScript Bundle with Popper -->
