10f91e24a4e75d0a99bbccdc44a5d7847786ade3
davidgm22 <davidgutierrez1997@gmail.com>
2022-11-03 21:09:06 -0600

    Users Monitor completado, Logs monitor en desarrollo

--- Frontend/logsMonitor.php
+++ Frontend/logsMonitor.php
@@ -1,26 +1,11 @@
 <?php
-    include_once('Oracle.php');
-
-
-    $stid = oci_parse($conn,'select STATUS from v$log');
-    $sto = oci_parse($conn,'select LOG_MODE from V$DATABASE');
-   
-    oci_execute($stid);
-    $resultado = oci_execute($stid);
-    oci_execute($sto);
-    $resultado = oci_execute($sto);
-
-    $query= oci_parse($conn,'begin :result := switch_minutes_avg; end;');
-
-    oci_bind_by_name($query, ':result', $result, 40);
-
-    oci_execute($query);
-
-    oci_free_statement($query);
-
-    oci_close($conn);
+$jsonlogs = file_get_contents("http://localhost/Oracle_Monitor_SGA_TableSpce/server/results.php?q=logsinfo");
+$jsonmode = file_get_contents("http://localhost/Oracle_Monitor_SGA_TableSpce/server/results.php?q=logmode");
+$jsonmin = file_get_contents("http://localhost/Oracle_Monitor_SGA_TableSpce/server/results.php?q=logsswitch");
+$logsinfo = json_decode($jsonlogs);
+$mode = json_decode($jsonmode);
+$switchminutes = json_decode($jsonmin);
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -31,14 +16,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="refresh" content="10" >
     <title>SGA Size Monitor</title>
-    <link rel="stylesheet" href="tableuser.css">
-    <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
         crossorigin="anonymous"></script>
-    <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/userS.css">
+
 </head>
 
 <body>
@@ -47,12 +31,7 @@
             <div class="header">
                 <div class="box1">
                     <p class="titu">Logs Status Monitor</p>
-                    <img class="logo" src="logo.png">
-                    <div class="button">
-                        <a href="menu.html">
-                            <button class="boton">Back to Menu</button>
-                        </a>
-                    </div>
+                    <img class="logo" src="img/logo.png">
                 </div>
             </div>
         </div>
@@ -63,17 +42,13 @@
         <div class="form-control">
             <br><div class="table-responsive">
                 <table class="table">
+                    <thead cGROUPtext-muted">
                     <thead class="text-muted">
+                        <th>GROUP</th>
+                        <th>MEMBERS</th>
                         <th>STATUS</th>
-            
                     </thead> 
-
                     <tbody>
-                        <?php while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)){?>
-                            <tr>
-                            <td><?php $row['STATUS']; if($row['STATUS']=="CURRENT"){?> <img src="currentLog.png" alt="Trulli" width="100" height="100"> <?php }else{?> <img src="zzzLog.png" alt="Trulli" width="100" height="100"> <?php } ?></td>
-                            </tr>
-                        <?php } ?>
                     </tbody>              
                 </table>
             </div>
@@ -83,15 +58,12 @@
             <br><div class="table-responsive">
                 <table class="table">
                     <thead class="text-muted">
-                        <th>Switch minutes </th>
-            
+                        <th>Switch Log (minutes) </th>
                     </thead>    
                     <tbody>
-                            
-                            <tr>
-                            <td><?php echo $result; ?></td>
-                            </tr>
-                           
+                        <tr>
+                            <td><?php echo $switchminutes; ?></td>
+                        </tr>
                     </tbody>                  
                 </table>
             </div>
@@ -102,21 +74,20 @@
                 <table class="table">
                     <thead class="text-muted">
                         <th>Log Mode</th>
-            
                     </thead>    
-                    <tbody>
-                            
-                        <?php while($row = oci_fetch_array($sto, OCI_ASSOC+OCI_RETURN_NULLS)){?>
-                            <tr>
-                                <td><?php echo $row['LOG_MODE']; ?></td>
-                            </tr>
-                        <?php } ?>
-                           
+                    <tbody> 
+                        <tr>
+                            <td><?php echo $mode; ?></td>
+                        </tr>                       
                     </tbody>                  
                 </table>
             </div>
         </div>
-
+        <div class="button">
+            <a href="menu.html">
+                <button class="boton">Back to Menu</button>
+            </a>
+        </div>
     </div>
 </body>
 
